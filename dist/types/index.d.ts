e8dcb76bba230831115288aba0576ba3f8e71c11
tqma <tqma@ctrip.com>
2019-08-07 17:36:22 +0800

    build

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,13 +1,18 @@
 export declare const locationsAreEqual: (a: import("./LocationUtils").Location, b: import("./LocationUtils").Location) => boolean;
-export { default as createHistory } from './createBrowserHistory';
-export { default as createHashHistory } from './createHashHistory';
-export { default as createMemoryHistory } from './createMemoryHistory';
-export { default as useBasename } from './useBasename';
-export { default as useBeforeUnload } from './useBeforeUnload';
-export { default as useQueries } from './useQueries';
-export { default as Actions } from './Actions';
+import { default as Actions } from './Actions';
 export { Work, Callback } from './AsyncUtils';
 export { PathCoder, PathCoders } from './createHashHistory';
 export { GetCurrentLocationFunc, HistoryOptions, NativeHistory, CreateHistoryFunc } from './createHistory';
 export { Memo } from './createMemoryHistory';
 export { Location } from './LocationUtils';
+declare const _default: {
+    locationsAreEqual: (a: import("./LocationUtils").Location, b: import("./LocationUtils").Location) => boolean;
+    createHistory: import("./createHistory").CreateHistoryFunc;
+    createHashHistory: import("./createHistory").CreateHistoryFunc;
+    createMemoryHistory: import("./createHistory").CreateHistoryFunc;
+    useBasename: (createHistory: import("./createHistory").CreateHistoryFunc) => (options: import("./createHistory").HistoryOptions) => import("./createHistory").NativeHistory;
+    useBeforeUnload: (createHistory: import("./createHistory").CreateHistoryFunc) => (options: import("./createHistory").HistoryOptions) => import("./createHistory").NativeHistory;
+    useQueries: (createHistory: import("./createHistory").CreateHistoryFunc) => (options?: import("./createHistory").HistoryOptions) => import("./createHistory").NativeHistory;
+    Actions: typeof Actions;
+};
+export default _default;
