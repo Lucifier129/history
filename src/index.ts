4cf38f9af6e940f4f1f996c82bc82c58d6ef00d9
tqma <tqma@ctrip.com>
2019-09-20 16:01:17 +0800

    fix

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as Types from './type'
+
+export { default as createBrowserHistory } from './createBrowserHistory'
+
+export { default as createHashHistory } from './createHashHistory'
+
+export { default as createMemoryHistory } from './createMemoryHistory'
+
+export { MemoryOptions } from './createMemoryHistory'
+
+export type HistoryOptions = Types.HistoryOptions
